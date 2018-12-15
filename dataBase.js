728e435258baf1610ef9c89cac003282cf5c5c38
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-12-15 17:58:01 +0300

    Добавила описание БД.

--- dataBase.js
+++ dataBase.js
@@ -1,4 +1,24 @@
-const MongoClient = require("mongodb").MongoClient;
+/*
+Всего в базе данных храниться 3 коллекции:
+ - user - хранит информацию о зарегестрированных пользователях.
+   + _id - идентификатор (генерируется MongoDB);
+   + login - логин пользователя;
+   + password - пароль пользователя;
+   + email - электронная почта пользователя.
+- book - хранит информацию о книгах.
+   + _id - идентификатор (генерируется MongoDB);
+   + name - название книги;
+   + author - автор книги;
+   + annotation - аннотация книги (краткое содержимое);
+   + description - описание атрибутов книги: год издания, издательство, количество страниц и др.
+- order - хранит какой пользователь заказал какую книгу и когда.
+   + _id - идентификатор (генерируется MongoDB);
+   + id_user - идентификатор пользователя, который сделал заказ;
+   + id_book - идентификатор книги, которую заказали;
+   + date - дата заказа.
+*/
+
+const MongoClient = require("mongodb").MongoClient; //модуль для работы с MongoDB
 var dbURL = "mongodb://localhost:27017";
 var dataBase = null; //будет хранить объект базы данных
 const mongoClient = new MongoClient(dbURL, { useNewUrlParser: true });
@@ -13,6 +33,7 @@ mongoClient.connect(function(err, db){
 
 //функция добавления нового пользователя (вызывается при регестрациии)
 //в функцию передаются: log - логин нового пользователя; pass - пароль нового пользователя; mail - адресс электронной почты нового пользователя
+//функция ничего не возвращает
 function addToUser(log, pass, mail){
 	var newData = {login: log, password: pass, email: mail}; //формирование объекта с данными о новом пользователе
 	mongoClient.connect(function(err, db){
@@ -32,6 +53,7 @@ function addToUser(log, pass, mail){
 
 //функция, проверяющая есть ли в коллекции user пользователь с переданным логином и паролем
 //в функцию передаются: log - логин предполагаемого пользователя; pass - пароль предпалогаемого пользователя
+//возвращает запись (в MongoDB называется документом) о пользователе если он есть, иначе - null
 function getUserByLoginAndPassword(log, pass){
 	var query = {login: log, password: pass};
 	mongoClient.connect(function(err, db){
