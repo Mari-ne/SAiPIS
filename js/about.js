d3b1168238023b7ff06d482bab677251eab16a22
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-12-16 22:15:41 +0300

    Update about.js

--- js/about.js
+++ js/about.js
@@ -4,8 +4,12 @@
 		method: 'get',
 		datatype: "json",
 		success: function(response){
+			//response содержит массив JS Object'ов, которые хранят объект типа:
+			// type: 
+			// value: 
 			console.log(response);
 			response.map(function(item){
+				//для каждого элемента массива создается элемент (li) в списке(#contacts)
 				$("#contacts").append($("<li></li>").text(item.type + ":  " + item.value));
 			});
 			
