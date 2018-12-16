484bb21c7c9c7b014e39a9720a5a07e13e63fcf5
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-12-16 22:17:14 +0300

    Update catalogScript.js

--- js/catalogScript.js
+++ js/catalogScript.js
@@ -16,5 +16,7 @@ $.ajax({
 
 
 $("#about").click(function(){
+	//при нажатии на кнопку #about происходит переход по адресу localhost:12321/about
+	//новая страница открывается в той же вкладке (параметр _self)
 	window.open('http://localhost:12321/about', '_self');
 })
