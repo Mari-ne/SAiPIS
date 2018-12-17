8ef7bf1d1a5b24bb838cb0b0c215c750c9cf63af
LizaSamodumkina <43859393+LizaSamodumkina@users.noreply.github.com>
2018-12-17 18:03:50 +0300

    Update catalogScript.js

--- js/catalogScript.js
+++ js/catalogScript.js
@@ -4,16 +4,39 @@ $.ajax({
     success: function(response){
         document.getElementById("insertBody").innerHTML += response;
 		$("button[id^='but_']").click(function () {
-					alert("yes");
-			});
+		    var current_but_id = this.id;
+		    var index = current_but_id.substring(4, current_but_id.length);
+		    var bookId = "book" + index;
+		    var bookName = $("#"+bookId+"").html();
+            var authorId = "author" + index;
+            var authorName = $("#"+authorId+"").html();
+            if (sessionStorage.getItem("login") ==  null){
+                //нет авторизированного пользователя
+
+            }else{
+                var config = {}; //это объект
+                config["user"] = sessionStorage.getItem("login");
+                config["bookName"] = bookName;
+                config["authorName"] = authorName;
+                $.ajax({
+                    url: 'http://localhost:12321/makeOrder',
+                    method: 'get',
+                    data: config,
+                    success: function (response) {
+                        console.log("eee");
+                    },
+                    error: function (response) {
+                        console.log("error");
+                    }
+                });
+            }
+		});
     },
     error: function(result){
         console.log(result);
         console.log("error");
     }
 });
-
-
 
 $("#about").click(function(){
 	//при нажатии на кнопку #about происходит переход по адресу localhost:12321/about
